f02a4cd4ff676461c8db0cf0cdf0f0b10ed68d12
KCBierlich <kcb43@duke.edu>
2021-07-06 09:12:59 -0700

    updated

--- R/subplans/create_plots_plan.R
+++ R/subplans/create_plots_plan.R
@@ -1,17 +1,25 @@
 create_plots_plan <- drake_plan(
-  Sc1_figures = target(
+  
+  Sc1_figures = {
     rmarkdown::render(
-      knitr_in(!!file.path('reports', 'Sc1_figures.Rmd')),
-      #output_file = file_out("Sc1_output_figures.html"),
-    output_dir = file.path('output', 'reports'),
-    quiet = FALSE)),
-
-
-# for uncorrected figures 
-Uncorrected_error_figures = target(
+      knitr_in("reports/Sc1_figures.Rmd"),
+      output_dir = 'output/reports',
+      output_file = "Sc1_output_figures.html",
+      quiet = FALSE
+      )
+    file_out("output/reports/Sc1_output_figures.html")
+  },
+    
+    
+  # for uncorrected figures 
+  Uncorrected_error_figures = {
     rmarkdown::render(
-      knitr_in(!!file.path('reports', 'uncorrected_error_plots.Rmd')), 
-      output_file = file_out("Uncorrected_error_figures.html"),
-      output_dir = file.path('output', 'reports'),
-      quiet = FALSE))
+      knitr_in("reports/'uncorrected_error_plots.Rmd"),
+      output_dir = 'output/reports',
+      output_file = "Uncorrected_error_figures.html",
+      quiet = FALSE
+      )
+    file_out("output/reports/Uncorrected_error_figures.html")
+  }
 )
+
